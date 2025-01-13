13e51e727156b5dc1c66f636b32d9f9bc95f2797
Dushenko <Dushenko@broccolidev.ru>
2025-01-13 09:27:39 +0700

    dry run flag added

--- src/Command/ParseKoreanWebsiteCommand.php
+++ src/Command/ParseKoreanWebsiteCommand.php
@@ -6,7 +6,9 @@ use App\Client\EncarApiClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
 
 class ParseKoreanWebsiteCommand extends Command
 {
@@ -17,7 +19,8 @@ class ParseKoreanWebsiteCommand extends Command
         'Chevrolet (GM Daewoo)' => '쉐보레(GM대우)',
         'Renault Korea (Samsung)' => '르노코리아(삼성)',
         'KG Mobility (SsangYong)' => 'KG모빌리티(쌍용)',
-        'Other Manufacturers' => '기타 제조사', ];
+        'Other Manufacturers' => '기타 제조사',
+    ];
     private $client;
 
     public function __construct(EncarApiClient $client)
@@ -29,7 +32,8 @@ class ParseKoreanWebsiteCommand extends Command
     protected function configure(): void
     {
         $this->setName('parse:korean-website')
-            ->addArgument('context', InputArgument::REQUIRED)
+            ->addArgument('context', InputArgument::REQUIRED, 'The context for fetching cars')
+            ->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Run the command without processing data')
             ->setDescription('Parse Korean website data');
     }
 
@@ -38,25 +42,44 @@ class ParseKoreanWebsiteCommand extends Command
         $carManufacturers = array_flip(self::CARMAP);
 
         $context = $input->getArgument('context');
+        $dryRun = $input->getOption('dry-run');
+
+        $startTime = microtime(true);
 
         try {
+            $data = $this->client->fetchCars($context, 8, 8);
 
-            $data = $this->client->fetchCars($context, 40, 100);
+            $table = new Table($output);
+            $table->setHeaders(['Car ID', 'Model', 'Badge', 'Year', 'Price', 'Brand']);
 
+            foreach ($data as $car) {
+                $table->addRow([
+                    $car['Id'],
+                    $car['Model'],
+                    $car['Badge'],
+                    $car['Year'],
+                    $car['Price'],
+                    $carManufacturers[$car['Manufacturer']],
+                ]);
+            }
 
-                foreach ($data as $car) {
-                    $output->writeln(sprintf(
-                        'Car ID: %s, Model: %s, Badge: %s, Year: %s, Price: %s, Brand: %s',
-                        $car['Id'],
-                        $car['Model'],
-                        $car['Badge'],
-                        $car['Year'],
-                        $car['Price'],
-                        $carManufacturers[$car['Manufacturer']],
-                    ));
-                }
+            $table->render();
+
+            $endTime = microtime(true);
+            $executionTime = $endTime - $startTime;
+
+            $output->writeln(sprintf(
+                '<comment>Total elements: %d</comment>',
+                count($data)
+            ));
+            $output->writeln(sprintf(
+                '<comment>Execution time: %.4f seconds</comment>',
+                $executionTime
+            ));
+
         } catch (\Exception $e) {
-            $output->writeln('Error occurred: ' . $e->getMessage());
+            $output->writeln('<error>Error occurred: ' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
         }
 
         return Command::SUCCESS;
